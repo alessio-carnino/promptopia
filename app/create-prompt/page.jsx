98bf7eef99487e98d719ca66e1aa649b1e608b27
Alessio <carninoalessio@gmail.com>
2024-03-05 13:56:16 +0100

    Submit prompt page and form

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -8,12 +8,34 @@ import Form from "@components/Form";
 
 const CreatePrompt = () => {
   const [submitting, setSubmitting] = useState(false);
-  const [postMessage, setPost] = useState({
+  const [post, setPost] = useState({
     prompt: "",
     tag: "",
   });
 
-  const createPrompt = async (e) => {};
+  const createPrompt = async (e) => {
+    e.preventDefault();
+    setSubmitting(true);
+
+    try {
+      const response = await fetch("api/prompt/new", {
+        method: "POST",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag,
+        }),
+      });
+
+      if (response.ok) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setSubmitting(false);
+    }
+  };
 
   return (
     <Form
