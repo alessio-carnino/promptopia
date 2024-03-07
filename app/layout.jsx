447bd5ab0b7ca7eef24b95a36dc4bc738504ae7f
Alessio <carninoalessio@gmail.com>
2024-03-07 12:24:19 +0100

    Favicon and warning fix

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,10 +6,17 @@ import Provider from "@components/Provider";
 export const metadata = {
   title: "Promptopia",
   descriptions: "Discoverr & Share AI Prompts",
+  icons: {
+    icon: ["/favicon.ico?v=4"],
+    apple: ["/apple-touch-icon.png?v=4"],
+    shortcut: ["/apple-touch.icon.png"],
+  },
+  manifest: "/site.webmanifest",
 };
+
 const RootLayout = ({ children }) => {
   return (
-    <html Lang="en">
+    <html lang="en">
       <body>
         <Provider>
           <div className="main">
