fb85dcdd774d3fe567c549f71505b12a3197dae6
Alessio <carninoalessio@gmail.com>
2024-03-12 15:59:35 +0100

    fic cache 3 and sort

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -5,15 +5,14 @@ export const GET = async (request, res) => {
   try {
     await connectToDB();
 
-    const prompts = await Prompt.find({})
-      .sort({ updatedAt: "desc" })
-      .populate("creator");
+    const prompts = await Prompt.find({}).sort({ _id: -1 }).populate("creator");
 
-    const response = Response(JSON.stringify(prompts), {
-      status: 200,
-    });
+    // return new Response(JSON.stringify(prompts), { status: 200 });
+
+    const response = new Response(JSON.stringify(prompts), { status: 200 });
     // with the following line the cache won't work and every time I refresh the page there is a new api call
-    response.setHeader("Cache-Control", "max-age=0, s-maxage=0");
+    // await response.setHeader("Cache-Control", "max-age=0, s-maxage=0");
+    response.headers.set("Cache-Control", "max-age=0, s-maxage=0");
     return response;
   } catch (error) {
     return new Response("Failed to fetch all prompts.", { status: 500 });
