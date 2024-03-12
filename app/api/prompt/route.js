d9fabb4bbe598261744172b7a896e09432ae62bb
Alessio <carninoalessio@gmail.com>
2024-03-12 16:24:13 +0100

    fix cache 4

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,19 +1,15 @@
 import { connectToDB } from "@utils/database";
 import Prompt from "@models/prompt";
 
-export const GET = async (request, res) => {
+export const GET = async (request) => {
   try {
     await connectToDB();
 
     const prompts = await Prompt.find({}).sort({ _id: -1 }).populate("creator");
-
-    // return new Response(JSON.stringify(prompts), { status: 200 });
-
-    const response = new Response(JSON.stringify(prompts), { status: 200 });
-    // with the following line the cache won't work and every time I refresh the page there is a new api call
-    // await response.setHeader("Cache-Control", "max-age=0, s-maxage=0");
-    response.headers.set("Cache-Control", "max-age=0, s-maxage=0");
-    return response;
+    return new Response(JSON.stringify(prompts), {
+      status: 200,
+      headers: { "Cache-Control": "max-age=0, s-maxage=0" }, // with the following line the cache won't work and every time I refresh the page there is a new api call
+    });
   } catch (error) {
     return new Response("Failed to fetch all prompts.", { status: 500 });
   }
